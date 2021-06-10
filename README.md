77f29e4d4fe88d0ecd37c73ad853eb386c037ef2
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-06-10 16:47:04 +0100

    transfer data from datacake to database

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# flairbods
+# Airbods
+
+# Testing
+
+See [Testing a DAG](https://airflow.apache.org/docs/apache-airflow/stable/best-practices.html#testing-a-dag).
+
+To check that the DAG code may be imported:
+
+```bash
+docker-compose exec worker python dags/datacake.py
+```
+
+Test a specific task:
+
+```bash
+# docker-compose exec <service> airflow task test <dag_id> <task_id> <date>
+docker-compose exec worker airflow tasks test datacake all_devices_history 2021-06-01
+```
+
+Run unit tests:
+
+```bash
+docker-compose exec worker python -m unittest --failfast
+```
+
--- dags/datacake.py
+++ dags/datacake.py
@@ -2,27 +2,31 @@ import datetime
 import textwrap
 
 import airflow.models
-from airflow.operators.bash import BashOperator
 
 from operators.graphql import GraphQLHttpOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-DATACAKE_WORKSPACE_ID = airflow.models.Variable.get(
-    'datacake_workspace_id')
+
+def bulk_load_values(*args, task_instance, **kwargs):
+    data = task_instance.xcom_pull('all_devices_history')
+
 
 with airflow.DAG(
-        dag_id='airbods',
+        dag_id='datacake',
         start_date=datetime.datetime(2021, 5, 17,
                                      tzinfo=datetime.timezone.utc),
         schedule_interval=datetime.timedelta(hours=1),
 ) as dag:
     # Download raw data for all devices
     get_raw_data = GraphQLHttpOperator(
-        http_conn_id='datacake_airbods',
+        http_conn_id='datacake',
         task_id='all_devices_history',
         # Jinja escape characters for GraphQL syntax
         query=textwrap.dedent("""
         query {{ '{' }}
-          allDevices(inWorkspace: "{{ DATACAKE_WORKSPACE_ID }}") {{ '{' }}
+          allDevices(inWorkspace: "{{ var.value.datacake_workspace_id }}") {{ '{' }}
             history(
               fields: ["CO2","TEMPERATURE","LORAWAN_SNR","LORAWAN_DATARATE","LORAWAN_RSSI","AIR_QUALITY","HUMIDITY"]
               timerangestart: "{{ ts }}"
@@ -31,15 +35,13 @@ with airflow.DAG(
             ) 
           {{ '}' }}
         {{ '}' }}
-        """)
+        """),
     )
 
-    # Write the result of previous task to file
-    save_raw_data = BashOperator(
-        task_id='save_data',
-        bash_command=textwrap.dedent("""
-        echo {{ ti.xcom_pull("raw_{{ id }}") }} > /{{ id }}.json
-        """)
+    bulk_load = PythonOperator(
+        task_id='bulk_load',
+        python_callable=bulk_load_values,
+        provide_context=True,
     )
 
-    get_raw_data >> save_raw_data
+    get_raw_data >> bulk_load
