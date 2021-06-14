31111c70d9a69dd7c52842d93a6ee849d2c6c8bc
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-06-14 15:17:22 +0100

    bulk insert readings

--- dags/datacake.py
+++ dags/datacake.py
@@ -1,6 +1,8 @@
 import datetime
 import textwrap
 import json
+import logging
+from typing import Iterable
 
 import airflow.models
 import psycopg2.extras
@@ -10,44 +12,73 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
+LOGGER = logging.getLogger(__name__)
+
+
+def flatten_history(devices: Iterable[dict]) -> Iterable[dict]:
+    row_count = 0
+    for device in devices:
+        for row in json.loads(device.pop('history')):
+            row_count += 1
+            yield dict(**device, **row)
+
+    LOGGER.info('Generated %s rows', row_count)
+
 
 def bulk_load_values(*args, task_instance, **kwargs):
-    data = task_instance.xcom_pull('all_devices_history')
+    # Get result of previous task
+    response = task_instance.xcom_pull('all_devices_history')
+    response_data = json.loads(response)
 
     # Convert GraphQL response to data rows
-    rows = (
-        dict(
-            device_id=device['id'],
-            **json.loads(device['history'])
-        )
-        for device in data['data']['allDevices'])
+    rows = flatten_history(response_data['data']['allDevices'])
 
+    # Connect to target database
     hook = PostgresHook('database')
-
     connection = hook.get_conn()
 
+    # Bulk insert values
     # https://hakibenita.com/fast-load-data-python-postgresql#execute-values-from-iterator-with-page-size
     with connection.cursor() as cursor:
         psycopg2.extras.execute_values(
             cur=cursor,
-            sql="INSERT INTO airbods.public.raw VALUES %s;",
-            arglist=((
-                row['device_id'],
+            sql="""
+            INSERT INTO airbods.public.raw (
+                 device_id        
+                ,time_
+                ,air_quality
+                ,co2  
+                ,humidity         
+                ,temperature      
+                ,lorawan_datarate 
+                ,lorawan_rssi     
+                ,lorawan_snr      
+                ,battery          
+                ,pm1              
+                ,pm25             
+                ,pm10             
+            )
+            VALUES %s;
+            """,
+            argslist=((
+                row['id'],
                 row['time'],
-                row.get('air_quality'),
-                row.get('co2'),
-                row.get('humidity'),
-                row.get('temperature'),
-                row.get('lorawan_datarate'),
-                row.get('lorawan_rssi'),
-                row.get('lorawan_snr'),
-                row.get('battery'),
-                row.get('pm1'),
-                row.get('pm25'),
-                row.get('pm10'),
+                row.get('AIR_QUALITY'),
+                row.get('CO2'),
+                row.get('HUMIDITY'),
+                row.get('TEMPERATURE'),
+                row.get('LORAWAN_DATARATE'),
+                row.get('LORAWAN_RSSI'),
+                row.get('LORAWAN_SNR'),
+                row.get('BATTERY'),
+                row.get('PM1'),
+                row.get('PM25'),
+                row.get('PM10'),
             ) for row in rows),
             page_size=1000,
         )
+
+    connection.commit()
 
 
 with airflow.DAG(
