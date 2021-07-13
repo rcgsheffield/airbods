e3873ded761b9332add078238d69f880a8276ee8
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-07-13 16:26:35 +0100

    add MATLAB example

---
+++ examples/matlab/airbods.m
@@ -0,0 +1,17 @@
+% Get Airbods data from the PostgreSQL database
+
+% You'll need to install MATLAB Database Toolbox
+% https://uk.mathworks.com/products/database.html
+
+% Configure PostgreSQL Native Interface Data Source
+% https://uk.mathworks.com/help/database/ug/configure-postgresql-native-interface-data-source.html
+
+% Import Data from PostgreSQL Database Table
+% https://uk.mathworks.com/help/database/ug/import-data-from-postgresql-database-table.html
+
+datasource = "Airbods Dev";
+username = "airbods";
+conn = postgresql(datasource, username, 'PASSWORD');
+tablename = "device";
+data = sqlread(conn, tablename);
+disp(data)
