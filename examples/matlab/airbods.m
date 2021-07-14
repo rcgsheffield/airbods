921d160a4b260217843764b8eaf8c2349f54a7c2
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-07-14 16:47:35 +0100

    use scram encrpytion

--- examples/matlab/airbods.m
+++ examples/matlab/airbods.m
@@ -9,9 +9,12 @@
 % Import Data from PostgreSQL Database Table
 % https://uk.mathworks.com/help/database/ug/import-data-from-postgresql-database-table.html
 
+% Connect to the database
 datasource = "Airbods Dev";
 username = "airbods";
 conn = postgresql(datasource, username, 'PASSWORD');
+
+% Explore the schema
 tablename = "device";
 data = sqlread(conn, tablename);
 disp(data)
