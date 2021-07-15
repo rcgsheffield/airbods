8ca83b2bf15861a534a89a47d27a59267aab41ba
Joe Heffer <j.heffer@sheffield.ac.uk>
2021-07-15 11:32:57 +0100

    get readings in MATLAB

--- examples/matlab/airbods.m
+++ examples/matlab/airbods.m
@@ -1,21 +1,45 @@
-% Get Airbods data from the PostgreSQL database
+%{
+Get Airbods data from the PostgreSQL database
 
-% You'll need to install MATLAB Database Toolbox
-% https://uk.mathworks.com/products/database.html
+You'll need to install MATLAB Database Toolbox
+https://uk.mathworks.com/products/database.html
 
-% Configure PostgreSQL Native Interface Data Source
-% https://uk.mathworks.com/help/database/ug/configure-postgresql-native-interface-data-source.html
+Configure PostgreSQL Native Interface Data Source
+https://uk.mathworks.com/help/database/ug/configure-postgresql-native-interface-data-source.html
 
-% Import Data from PostgreSQL Database Table
-% https://uk.mathworks.com/help/database/ug/import-data-from-postgresql-database-table.html
+Import Data from PostgreSQL Database Table
+https://uk.mathworks.com/help/database/ug/import-data-from-postgresql-database-table.html
+%}
 
 % Connect to the database
 datasource = "Airbods Dev";
 username = "airbods";
-password = input('Enter password: ', 's');
+password = input("Enter password: ", "s");
 conn = postgresql(datasource, username, password);
 
 % Explore the schema
-tablename = "device";
-data = sqlread(conn, tablename);
-disp(data)
+
+% Get details of each device
+devices = sqlread(conn, "device");
+%disp(devices);
+
+% Download data
+% Define SQL statement as multiline string
+% https://uk.mathworks.com/matlabcentral/answers/21308-dealing-with-multiline-text#answer_28088
+sql = sprintf('%s\n', [
+"SELECT",
+"   serial_number",
+"  ,verbose_name",
+"  ,time_europe_london",
+"  ,area",
+"  ,room",
+"  ,air_quality",
+"  ,co2",
+"  ,humidity",
+"  ,temperature",
+"FROM reading",
+"WHERE time_europe_london BETWEEN '2021-07-09' AND '2021-07-10'",
+"  AND site LIKE '%Crucible%'"
+]);
+readings = fetch(conn, sql);
+%disp(readings);
